4aca2a11c6a50ed3d8f7ce24ce6c7f5f154db8a7
Perg01 <jsatshs@gmail.com>
2024-11-13 16:47:30 -0800

    Update connect todos api route to createtodo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { registerUser, loginUser } from './controllers/userController.js';
+import { createTodo } from './controllers/todoController.js';
 
 dotenv.config();
 const app = express();
@@ -9,10 +10,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
 app.post('/api/register', registerUser);
 app.post('/api/login', loginUser);
-
+app.post('/api/todos', createTodo);
 
 
 export default app;
